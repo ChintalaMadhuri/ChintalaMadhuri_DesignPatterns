404483a1d65fe413febfaa9bb07ada2f013b22e7
Ch Madhuri <madhurich2000@gmail>
2020-03-12 22:31:53 +0530

    Files added

---
+++ DesignPatterns/src/main/java/BehavioralPatterns/Addition.java
@@ -0,0 +1,16 @@
+package BehavioralPatterns;
+
+
+public class Addition implements Strategy{  
+	
+
+  
+	public float calculation(float a, float b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	
+	 
+  
+}
---
+++ DesignPatterns/src/main/java/BehavioralPatterns/I_Division.java
@@ -0,0 +1,9 @@
+package BehavioralPatterns;
+
+public class I_Division implements Strategy {
+
+	public float calculation(float a, float b) {
+		// TODO Auto-generated method stub
+		return a/b;
+	} 
+}
